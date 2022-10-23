625330d7a3c58fd935aa9207e44e5f534e5242c1
TayoAdekoya <114704525+TayoAdekoya@users.noreply.github.com>
2022-10-23 16:00:34 -0600

    Add files via upload

--- App Service/Linux_appserviceplan.tf
+++ App Service/Linux_appserviceplan.tf
@@ -1,46 +1,30 @@
 # creates a resource group
-resource "azurerm_resource_group" "LASP" {
- name     = "App-Service-Plan"
- location = "West US" 
+resource "azurerm_resource_group" "var.resource_group_deployment_name" {
+ name     = var.resource_group_name
+ location = var.resource_group_location
 }
 
 # creates App service plans
-resource "azurerm_service_plan" "LSP0" {
-  name                = "App-serviceplan-0"
-  location            = azurerm_resource_group.LASP.location
-  resource_group_name = azurerm_resource_group.LASP.name
-  os_type             = "Linux"
-  sku_name            = "F1"
-
-  #F1 = 1 GB of memory, 60 min/day compute, free
+resource "azurerm_service_plan" "var.service_plan_deployment_name" {
+  name                = var.Service_plan_name
+  location            = var.Service_plan_location
+  resource_group_name = var.Service_plan_resource_group_name
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
 
-resource "azurerm_service_plan" "LSP1" {
-  name                = "App-serviceplan-1"
-  location            = azurerm_resource_group.LASP.location
-  resource_group_name = azurerm_resource_group.LASP.name
-  os_type             = "Linux"
-  sku_name            = "B1"
+#Sku-names:
+#F1 = 1 GB of memory, 60 min/day compute, free
+#B1 = 100 total ACU (Azure_compute_units), 1.75 GB of memory, A-series compute equivalent, 13.14 USD/Month (Equivalent) 
+#B2 = 200 total ACU (Azure_compute_units), 3.5 GB of memory, A-series compute equivalent, 25.55 USD/Month (Equivalent)
 
-  #B1 = 100 total ACU (Azure_compute_units), 1.75 GB of memory, A-series compute equivalent, 13.14 USD/Month (Equivalent) 
-}
 
-resource "azurerm_service_plan" "LSP2" {
-  name                = "App-serviceplan-2"
-  location            = azurerm_resource_group.LASP.location
-  resource_group_name = azurerm_resource_group.LASP.name
-  os_type             = "Linux"
-  sku_name            = "B2"
-
-  #B2 = 200 total ACU (Azure_compute_units), 3.5 GB of memory, A-series compute equivalent, 25.55 USD/Month (Equivalent)
-}
-
-# creates Linux web app
-resource "azurerm_linux_web_app" "as_Linux" {
-  name                   = "asLinux-1"
-  resource_group_name    = azurerm_resource_group.LASP.name
-  location               = azurerm_resource_group.LASP.location
-  service_plan_id        = azurerm_resource_group.LASP.id
-
+# Creates windows web app
+resource "azurerm_linux_web_app" "var.web_app_deployment_name" {
+  name                   = var.web_app_name
+  location               = var.web_app_location
+  resource_group_name    = var.web_app_resource_group_name
+  service_plan_id        = var.service_plan_id
+  
   site_config {}
 }
--- App Service/Windows_appserviceplan.tf
+++ App Service/Windows_appserviceplan.tf
@@ -1,46 +1,30 @@
 # creates a resource group
-resource "azurerm_resource_group" "WASP" {
- name     = "App-Service-Plan"
- location = "West US" 
+resource "azurerm_resource_group" "var.resource_group_deployment_name" {
+ name     = var.resource_group_name
+ location = var.resource_group_location
 }
 
 # creates App service plans
-resource "azurerm_service_plan" "WSP0" {
-  name                = "App-serviceplan-0"
-  location            = azurerm_resource_group.WASP.location
-  resource_group_name = azurerm_resource_group.WASP.name
-  os_type             = "Windows"
-  sku_name            = "F1"
-
-  #F1 = 1 GB of memory, 60 min/day compute, free
+resource "azurerm_service_plan" "var.service_plan_deployment_name" {
+  name                = var.Service_plan_name
+  location            = var.Service_plan_location
+  resource_group_name = var.Service_plan_resource_group_name
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
 
-resource "azurerm_service_plan" "WSP1" {
-  name                = "App-serviceplan-1"
-  location            = azurerm_resource_group.WASP.location
-  resource_group_name = azurerm_resource_group.WASP.name
-  os_type             = "Windows"
-  sku_name            = "B1"
+#Sku-names:
+#F1 = 1 GB of memory, 60 min/day compute, free
+#B1 = 100 total ACU (Azure_compute_units), 1.75 GB of memory, A-series compute equivalent, 13.14 USD/Month (Equivalent) 
+#B2 = 200 total ACU (Azure_compute_units), 3.5 GB of memory, A-series compute equivalent, 25.55 USD/Month (Equivalent)
 
-  #B1 = 100 total ACU (Azure_compute_units), 1.75 GB of memory, A-series compute equivalent, 13.14 USD/Month (Equivalent) 
-}
-
-resource "azurerm_service_plan" "WSP2" {
-  name                = "App-serviceplan-2"
-  location            = azurerm_resource_group.WASP.location
-  resource_group_name = azurerm_resource_group.WASP.name
-  os_type             = "Windows"
-  sku_name            = "B2"
-
-  #B2 = 200 total ACU (Azure_compute_units), 3.5 GB of memory, A-series compute equivalent, 25.55 USD/Month (Equivalent)
-}
 
 # Creates windows web app
-resource "azurerm_windows_web_app" "as_Windows" {
-  name                   = "aswindows-1"
-  resource_group_name    = azurerm_resource_group.WASP.name
-  location               = azurerm_resource_group.WASP.location
-  service_plan_id        = azurerm_resource_group.WASP.id
-
+resource "azurerm_windows_web_app" "var.web_app_deployment_name" {
+  name                   = var.web_app_name
+  location               = var.web_app_location
+  resource_group_name    = var.web_app_resource_group_name
+  service_plan_id        = var.service_plan_id
+  
   site_config {}
 }
